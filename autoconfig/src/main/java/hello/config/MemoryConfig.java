b71c0f4c553bba36a2841f27c88f471e5865a16c
승호 <102205699+Drum-J@users.noreply.github.com>
2025-04-16 13:59:40 +0900

    Autoconfig 프로젝트 완료 (#4)
    * autoconfig 프로젝트 생성
    
    * 예제 만들기
    
    * 자동 구성 확인
    
    * 자동 구성 직접 만들기 - 기반 예제
    
    * @Conditional
    
    * @Conditional  - 다양한 기능

---
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -0,0 +1,25 @@
+package hello.config;
+
+import memory.MemoryCondition;
+import memory.MemoryController;
+import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@Conditional(MemoryCondition.class) // MemoryCondition 의 반환값이 true인 경우에만 이 MemoryConfig가 등록된다.
+@ConditionalOnProperty(name = "memory", havingValue = "on")
+public class MemoryConfig {
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+}
