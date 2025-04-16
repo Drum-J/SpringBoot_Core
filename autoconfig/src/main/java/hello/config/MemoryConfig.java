f702e39d98ac5403b97f011e92a7847fdcd4e4d2
승호 <tmdgh717@naver.com>
2025-04-16 13:36:13 +0900

    @Conditional

--- autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,11 +1,14 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Conditional(MemoryCondition.class) // MemoryCondition 의 반환값이 true인 경우에만 이 MemoryConfig가 등록된다.
 public class MemoryConfig {
 
     @Bean
