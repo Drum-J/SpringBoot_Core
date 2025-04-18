3e4b3d989203782f47c9505e16f6681f4aaeffa2
승호 <tmdgh717@naver.com>
2025-04-18 12:52:25 +0900

    설정 데이터1 - 내부 파일 분리

--- external/src/main/java/hello/EnvironmentCheck.java
+++ external/src/main/java/hello/EnvironmentCheck.java
@@ -19,7 +19,7 @@ public class EnvironmentCheck {
     public void init() {
         // program arguments / Add VM option 어디에 추가해도 읽을 수 있다!
         String url = env.getProperty("url");
-        String username = env.getProperty("username");
+        String username = env.getProperty("my_username"); //시스템 환경 변수로 username 이 잡혀있다. my_username 으로 변경
         String password = env.getProperty("password");
 
         log.info("env url = {}", url);
